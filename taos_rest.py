2935b9c709e3109a054329966302050eb43a7508
liufeng02 <liufeng02@inspur.com>
2022-11-15 18:35:30 +0800

    完成获取taos 数据库总数据量脚本,可配置命令行入参.

--- taos_rest.py
+++ taos_rest.py
@@ -1,10 +1,8 @@
 from taosrest import connect, TaosRestConnection, TaosRestCursor
+import sys, getopt
 
-def get_taos_data_size():
-    conn: TaosRestConnection = connect(url="http://10.64.6.57:6041",
-                                    user="root",
-                                    password="taosdata",
-                                    timeout=30)
+def get_taos_data_size(url:str,user:str,password:str,timeout:int):
+    conn: TaosRestConnection = connect(url=url,user=user,password=password,timeout=timeout)
     cursor: TaosRestCursor = conn.cursor()
 
     cursor.execute("SHOW xkjt.stables")
@@ -17,5 +15,46 @@ def get_taos_data_size():
         count_all += line_count
     print("taos_data,tag=test data_size="+str(count_all))
 
+def main(argv):
+    url = "http://localhost:6041"
+    user = "root"
+    password = "taosdata"
+    timeout = 30
+
+    try:
+        opts, args = getopt.getopt(argv,"hH:u:p:d:t:",["help","url=","user=","password=","timeout="])
+    except getopt.GetoptError:
+        show_help()
+        sys.exit(2)
+    if not opts:
+        show_help()
+    for opt, arg in opts:
+        if opt == '-h' or opt=='--help':
+            show_help() 
+            sys.exit()
+        elif opt == '-H' or opt == '--url':
+            url = arg
+        elif opt == '-u' or opt == '--user':
+            user = arg
+        elif opt == '-p' or opt == '--password':
+            password = arg
+        elif opt == '-t' or opt == '--timeout':
+            timeout = arg
+        else:
+            show_help()
+            sys.exit()
+    get_taos_data_size(url,user,password,timeout)
+
+
+def show_help():
+    print("Usage: python taos_rest.py [options]")
+    print("-h, --help show this help")
+    print("-H, --url urls of TDengine")
+    print("-u, --user user of TDengine,default:root")
+    print("-p, --password password of TDengine,default:taosdata")
+    print("-t, --timeout timeout of TDengine")
+
+
 if __name__ == "__main__":
-    get_taos_data_size()
+    # print(sys.argv)
+    main(sys.argv[1:])
