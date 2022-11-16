15005c9d023b0a1856b92ac9b177e5bcacd42802
liufeng02 <liufeng02@inspur.com>
2022-11-16 08:37:15 +0800

    获取taos 数据量脚本 增加measurement和field

--- taos_rest.py
+++ taos_rest.py
@@ -1,7 +1,7 @@
 from taosrest import connect, TaosRestConnection, TaosRestCursor
 import sys, getopt
 
-def get_taos_data_size(url:str,user:str,password:str,timeout:int):
+def get_taos_data_size(url:str,user:str,password:str,timeout:int,measurement:str,field:str):
     conn: TaosRestConnection = connect(url=url,user=user,password=password,timeout=timeout)
     cursor: TaosRestCursor = conn.cursor()
 
@@ -13,24 +13,27 @@ def get_taos_data_size(url:str,user:str,password:str,timeout:int):
         count = cursor.fetchall()
         line_count: int = 0 if not count or not count[0] else count[0][0]
         count_all += line_count
-    print("taos_data,tag=test data_size="+str(count_all))
+    print(measurement+",tag=taos_data "+field+"="+str(count_all))
 
 def main(argv):
     url = "http://localhost:6041"
     user = "root"
     password = "taosdata"
     timeout = 30
+    measurement = "taos_volume"
+    field = "data_size"
 
     try:
-        opts, args = getopt.getopt(argv,"hH:u:p:d:t:",["help","url=","user=","password=","timeout="])
+        opts, args = getopt.getopt(argv,"hH:u:p:d:t:m:f:",["help","url=","user=","password=","timeout=","measurement=","field="])
     except getopt.GetoptError:
         show_help()
         sys.exit(2)
     if not opts:
         show_help()
+        sys.exit(2)
     for opt, arg in opts:
         if opt == '-h' or opt=='--help':
-            show_help() 
+            show_help()
             sys.exit()
         elif opt == '-H' or opt == '--url':
             url = arg
@@ -40,10 +43,14 @@ def main(argv):
             password = arg
         elif opt == '-t' or opt == '--timeout':
             timeout = arg
+        elif opt == '-m' or opt == '--measurement':
+            measurement = arg
+        elif opt == '-f' or opt == '--field':
+            field = arg
         else:
             show_help()
             sys.exit()
-    get_taos_data_size(url,user,password,timeout)
+    get_taos_data_size(url,user,password,timeout,measurement,field)
 
 
 def show_help():
@@ -53,6 +60,10 @@ def show_help():
     print("-u, --user user of TDengine,default:root")
     print("-p, --password password of TDengine,default:taosdata")
     print("-t, --timeout timeout of TDengine")
+    print("-m, --measurement measurement of output")
+    print("-f, --field field of output")
+    
+
 
 
 if __name__ == "__main__":
